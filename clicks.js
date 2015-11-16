10a05e276a1d4df6c0d82b171469ee09a6a42106
kelvinma <me@kelvinma.com>
2015-11-16 10:37:00 -0500

    Created Click Handler

---
+++ clicks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+$(document).ready(function(){
+
+
+var userTemplate = Handlebars.compile($('#user-template').html());
+
+  var showUsers = function(error, data) {
+    if (error) {
+      console.error(error);
+    }
+    return;
+
+    var userHTML = userTemplate(data);
+
+      $('#users').html(userHTML);
+  };
+
+
+  // button click
+  $('#user-button').click(function(e){
+    e.preventDefault();
+    listUsers();
+  });
+
+});
